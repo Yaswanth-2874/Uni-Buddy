d2be2f62802f0c89cde77464dfaefcef4b0a20d8
Yaswanth <yryr2004@gmail.com>
2024-08-08 11:09:52 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
+});
