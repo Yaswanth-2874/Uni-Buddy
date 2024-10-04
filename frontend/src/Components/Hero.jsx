79e8cee48a22723c3923d86f9351a443af48232c
Hardik Chopra <hardikchopra70@gmail.com>
2024-10-04 09:15:14 +0000

    04-10-2024 changes3

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -81,13 +81,121 @@ function Hero() {
             </div>
           </div>
           <div className='bg-[#262d34] w-5/6 h-52 rounded-xl'>
-           
+           {/* paragraph */}
+           <div className='text-white p-4'>
+            What advice you give to incoming freshman about adjusting to college life?
+            </div>
+            {/* tags */}
+            <div className='flex gap-1'>
+              <div className='bg-[#232a30] rounded-full text-white p-3 ' >Freshman</div>
+              <div className='bg-[#232a30] rounded-full text-white p-3 '>College Life</div>
+              <div className='bg-[#232a30] rounded-full text-white p-3 '>Advice</div>
+            </div>
+            {/* username */}
+            <div className='flex justify-between m-4 text-white'>
+              {/* profile */}
+              <div className='flex'>
+                    {/* img */}
+                    <div>
+                      <img src="" alt="img" />
+                    </div>
+                    {/* name and online status */}
+                    <div className='flex flex-col'>
+                      <div>Aniketh</div>
+                      <div>3 weeks ago</div>
+                    </div>
+              </div>
+              {/* views */}
+              <div>
+                 651,324 Views
+              </div>
+              {/* likes */}
+              <div>
+                  36,6545 Likes   
+              </div>
+              {/* comments */}
+              <div>
+                  56 comments
+              </div>
+            </div>
           </div>
           <div className='bg-[#262d34] w-5/6 h-52 rounded-xl'>
-           
+              {/* paragraph */}
+           <div className='text-white p-4'>
+            What advice you give to incoming freshman about adjusting to college life?
+            </div>
+            {/* tags */}
+            <div className='flex gap-1'>
+              <div className='bg-[#232a30] rounded-full text-white p-3 ' >Freshman</div>
+              <div className='bg-[#232a30] rounded-full text-white p-3 '>College Life</div>
+              <div className='bg-[#232a30] rounded-full text-white p-3 '>Advice</div>
+            </div>
+            {/* username */}
+            <div className='flex justify-between m-4 text-white'>
+              {/* profile */}
+              <div className='flex'>
+                    {/* img */}
+                    <div>
+                      <img src="" alt="img" />
+                    </div>
+                    {/* name and online status */}
+                    <div className='flex flex-col'>
+                      <div>Aniketh</div>
+                      <div>3 weeks ago</div>
+                    </div>
+              </div>
+              {/* views */}
+              <div>
+                 651,324 Views
+              </div>
+              {/* likes */}
+              <div>
+                  36,6545 Likes   
+              </div>
+              {/* comments */}
+              <div>
+                  56 comments
+              </div>
+            </div>
           </div>
           <div className='bg-[#262d34] w-5/6 h-52 rounded-xl'>
-           
+           {/* paragraph */}
+           <div className='text-white p-4'>
+            What advice you give to incoming freshman about adjusting to college life?
+            </div>
+            {/* tags */}
+            <div className='flex gap-1'>
+              <div className='bg-[#232a30] rounded-full text-white p-3 ' >Freshman</div>
+              <div className='bg-[#232a30] rounded-full text-white p-3 '>College Life</div>
+              <div className='bg-[#232a30] rounded-full text-white p-3 '>Advice</div>
+            </div>
+            {/* username */}
+            <div className='flex justify-between m-4 text-white'>
+              {/* profile */}
+              <div className='flex'>
+                    {/* img */}
+                    <div>
+                      <img src="" alt="img" />
+                    </div>
+                    {/* name and online status */}
+                    <div className='flex flex-col'>
+                      <div>Aniketh</div>
+                      <div>3 weeks ago</div>
+                    </div>
+              </div>
+              {/* views */}
+              <div>
+                 651,324 Views
+              </div>
+              {/* likes */}
+              <div>
+                  36,6545 Likes   
+              </div>
+              {/* comments */}
+              <div>
+                  56 comments
+              </div>
+            </div>
           </div>
        </div>
        
